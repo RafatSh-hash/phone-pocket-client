4d44a4ec3c3702b1f222f61c628ae69e398691c0
Rafatsh-hash <rafatsh800@gmail.com>
2022-11-28 01:56:17 +0600

    seller verify added

--- src/Pages/AdminRoute/AllSellers/SellerCard.js
+++ src/Pages/AdminRoute/AllSellers/SellerCard.js
@@ -2,6 +2,7 @@ import { Button } from "flowbite-react";
 import React from "react";
 import "aos/dist/aos.css";
 import AOS from "aos";
+import { FaBeer, FaUserAlt, MdVerified } from "react-icons/fa";
 
 const SellerCard = ({ seller, refetch }) => {
   AOS.init({ duration: 500 });
@@ -21,6 +22,14 @@ const SellerCard = ({ seller, refetch }) => {
         }
       });
   };
+  const handleverifySeller = (seller) => {
+    console.log(seller);
+    fetch(`http://localhost:1000/sellers/verify/${seller._id}`, {
+      method: "PUT",
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
 
   return (
     <div
@@ -29,11 +38,17 @@ const SellerCard = ({ seller, refetch }) => {
     >
       <div className="flex justify-between w-3/5">
         <h3>{seller.name}</h3>
+
         <p>{seller.email}</p>
       </div>
       <div className="flex">
         <div className="mx-2">
-          <Button gradientMonochrome="teal">Verify</Button>
+          <Button
+            onClick={() => handleverifySeller(seller)}
+            gradientMonochrome="teal"
+          >
+            {<p>{seller?.status === "verified" ? "verified" : "Verify"}</p>}
+          </Button>
         </div>
         <div className="mx-2">
           <Button
